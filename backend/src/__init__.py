0419d4c03d5ce84bc1038b45ccb51ca0fdda9a11
eloisedang <101225765+eloisedang@users.noreply.github.com>
2024-05-15 11:21:26 +1000

    Update __init__.py

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -8,14 +8,14 @@ app.config.from_object('config.Config')  # Load configuration from config.py
 db = MySQL(app)
 
 # Import and register blueprints
-from routes.menu_routes import menu_blueprint
+from controllers.menu_routes import menu_blueprint
 app.register_blueprint(menu_blueprint, url_prefix='/menu')
 
-from routes.reservation_routes import reservation_blueprint
+from controllers.reservation_routes import reservation_blueprint
 app.register_blueprint(reservation_blueprint, url_prefix='/reservations')
 
-from routes.order_routes import order_blueprint
+from controllers.order_routes import order_blueprint
 app.register_blueprint(order_blueprint, url_prefix='/orders')
 
-from routes.feedback_routes import feedback_blueprint
+from controllers.feedback_routes import feedback_blueprint
 app.register_blueprint(feedback_blueprint, url_prefix='/feedbacks')
