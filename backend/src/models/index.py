84e60782e9d3188817b321114f7938bdc6c3aec8
eloisedang <101225765+eloisedang@users.noreply.github.com>
2024-05-15 14:01:17 +1000

    Delete backend directory

--- backend/src/models/index.py
+++
@@ -1,1 +0,0 @@
-# store all the logic (ie: classes)
--- backend/src/models/menu.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app as app
-
-def get_menu():
-    cursor = app.mysql.connection.cursor()
-    cursor.execute("SELECT * FROM menu")
-    items = cursor.fetchall()
-    cursor.close()
-    return items
