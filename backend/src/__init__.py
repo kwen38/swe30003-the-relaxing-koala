bcc11592156b13ccda67173aca86d8171650177b
eloisedang <101225765+eloisedang@users.noreply.github.com>
2024-05-15 12:00:15 +1000

    Update __init__.py

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -11,6 +11,64 @@ conn = mysql.connector.connect(
     database="koala_cafe"
 )
 
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS menu (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        items TEXT
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS reservations (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        phone VARCHAR(20),
+        reservation_date DATE,
+        reservation_time TIME,
+        party_size INT
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS orders (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        items TEXT,
+        total FLOAT,
+        timestamp DATETIME,
+        customer_name VARCHAR(255),
+        customer_email VARCHAR(255),
+        customer_phone VARCHAR(20),
+        in_restaurant BOOLEAN DEFAULT FALSE,  -- Boolean column for in-restaurant orders
+        status VARCHAR(255)
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS feedbacks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    rating INT,
+    comment TEXT,
+    timestamp DATETIME,
+    FOREIGN KEY (order_id) REFERENCES orders(id)
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        phone VARCHAR(20) NOT NULL,
+        role VARCHAR(50) NOT NULL
+    )
+ """)
+
+conn.commit()
+
 # Import and register blueprints
 from controllers.menu_routes import menu_blueprint
 app.register_blueprint(menu_blueprint, url_prefix='/menu')
