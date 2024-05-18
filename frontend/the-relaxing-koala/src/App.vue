550763035d14a6a9c84c407e0d919f6606657e46
Hong Quan Ngo <ngohongquan24462@gmail.com>
2024-05-18 18:57:37 +1000

    project init

---
+++ frontend/the-relaxing-koala/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import WelcomePage from "./views/WelcomePage.vue"
+</script>
+
+<template>
+  <WelcomePage />
+</template>
+
---
+++ frontend/the-relaxing-koala/src/views/WelcomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <button type="button" class="btn btn-secondary btn-lg mx-2">Login as Staff</button>
+    <button type="button" class="btn btn-secondary btn-lg mx-2">Continue as Guest</button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'WelcomePage'
+  }
+  </script>
+  
